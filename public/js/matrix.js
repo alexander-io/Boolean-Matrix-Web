c830bd5d2b11bf34ffbcefc88b351a8c480b8d8e
alexander-io <abharris@pugetsound.edu>
2017-08-18 09:16:28 -0700

    ++markdown

--- public/js/matrix.js
+++ public/js/matrix.js
@@ -2,7 +2,9 @@ let array_dimension = {
   w : 16,
   h : 16
 }
+
 let window_width = window.innerWidth, window_height = window.innerHeight, flag = false
+
 
 let init_matrix_array = (array_dimension) => {
   let matrix_array = []
@@ -18,14 +20,15 @@ let init_matrix_array = (array_dimension) => {
   return matrix_array
 }
 
+
 let draw_matrix = function(matrix_array) {
   let mount = document.getElementById('mount');
   for (let i = 0; i < matrix_array.length;i++){
     for (let j = 0; j < matrix_array[0].length;j++){
       let block = document.createElement('div');
       block.style.display = 'inline-block'
-      block.style.width = (window_width / matrix_array.length) - 1 + 'px'
-      block.style.height = window_height / matrix_array[0].length + 'px'
+      block.style.width = (window_width / matrix_array[0].length) - 1 + 'px'
+      block.style.height = window_height / matrix_array.length + 'px' // block.style.height = block.style.width
       matrix_array[i][j] ? block.style.backgroundColor = '#212121': block.style.backgroundColor = 'white'
       mount.appendChild(block)
     }
---
+++ readme.md
@@ -0,0 +1,2 @@
+# 2-Dimensional Boolean Matrix for the Web
+[2dmatrix](j2.png)
