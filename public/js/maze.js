7317fefc29f0adf25f9dd11f2baba723e487413c
alexander-io <abharris@pugetsound.edu>
2017-08-18 22:05:54 -0700

    ++twojsarrayonscreenyah

--- public/js/maze.js
+++ public/js/maze.js
@@ -11,7 +11,7 @@ var two = new Two({
   fill : 'black'
 }).appendTo(document.getElementById('maze-mount'));
 
-var rect = two.makeRectangle(0, 0 / 2, 50 ,50);
+// var rect = two.makeRectangle(0, 0 / 2, 50 ,50);
 
 let col = []
 
@@ -19,18 +19,22 @@ let dimension = {
   s:16
 }
 
+let block_width = (two.width/16)-2.6, block_height = (two.height/16)-2.6
+
+
 let make_matrix_of_rectangles = function(dimension) {
   let outer = []
-  for (let x = 0; x < dimension.s; x++) {
+  for (let x = 1; x <= dimension.s; x++) {
     let inner = []
-    for (let xx = 0; xx < dimension.x; xx++) {
-      inner.push(two.makeRectangle(two.width , two.height / 2, 50 ,50))
+    for (let xx = 1; xx <= dimension.s; xx++) {
+      inner.push(two.makeRectangle( block_width*xx, block_height*x, 50 ,50))
     }
+    outer.push(inner)
   }
 }
 
-
+make_matrix_of_rectangles(dimension)
 
 two.bind('update', function() {
-  rect.rotation += 0.001;
+  // rect.rotation += 0.001;
 });
