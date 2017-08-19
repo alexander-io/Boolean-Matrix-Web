991b41c12f79f633d56ae870a61b4d89fa85a2ec
Justin Brush <jbrush@pugetsound.edu>
2017-08-18 22:21:54 -0700

    Fixes maze's squares' dimensions

--- public/js/maze.js
+++ public/js/maze.js
@@ -1,7 +1,6 @@
-console.log('canary');
+// console.log('canary');
 let window_width = window.innerWidth, window_height = window.innerHeight
-console.log(window_width, window_height)
-
+// console.log(window_width, window_height)
 
 var two = new Two({
   fullscreen: false,
@@ -19,15 +18,14 @@ let dimension = {
   s:16
 }
 
-let block_width = (two.width/16)-2.6, block_height = (two.height/16)-2.6
-
+let block_size = (two.width / 16);
 
 let make_matrix_of_rectangles = function(dimension) {
   let outer = []
-  for (let x = 1; x <= dimension.s; x++) {
+  for (let x = 0; x < dimension.s; x++) {
     let inner = []
-    for (let xx = 1; xx <= dimension.s; xx++) {
-      inner.push(two.makeRectangle( block_width*xx, block_height*x, 50 ,50))
+    for (let xx = 0; xx < dimension.s; xx++) {
+      inner.push(two.makeRectangle( block_size*xx + (block_size/2), block_size*x + (block_size/2), block_size ,block_size))
     }
     outer.push(inner)
   }
