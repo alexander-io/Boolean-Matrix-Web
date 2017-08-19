4f35c7f683507ea2bce39621c66bc66d233651ff
alexander-io <abharris@pugetsound.edu>
2017-08-18 21:54:28 -0700

    ++resize canvas fullscreen->50% screen, color background, center element, uncomment path_forger original maze

--- public/js/maze.js
+++ public/js/maze.js
@@ -1,11 +1,35 @@
 console.log('canary');
+let window_width = window.innerWidth, window_height = window.innerHeight
+console.log(window_width, window_height)
+
 
 var two = new Two({
-  fullscreen: true,
-  autostart: true
-}).appendTo(document.body);
+  fullscreen: false,
+  autostart: true,
+  width : window_width/2,
+  height : window_width/2,
+  fill : 'black'
+}).appendTo(document.getElementById('maze-mount'));
 
-var rect = two.makeRectangle(two.width / 2, two.height / 2, 50 ,50);
+var rect = two.makeRectangle(0, 0 / 2, 50 ,50);
+
+let col = []
+
+let dimension = {
+  s:16
+}
+
+let make_matrix_of_rectangles = function(dimension) {
+  let outer = []
+  for (let x = 0; x < dimension.s; x++) {
+    let inner = []
+    for (let xx = 0; xx < dimension.x; xx++) {
+      inner.push(two.makeRectangle(two.width , two.height / 2, 50 ,50))
+    }
+  }
+}
+
+
 
 two.bind('update', function() {
   rect.rotation += 0.001;
